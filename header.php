74ed5ad282849b7d9dd473177057154a4db67728
Alex MOINIER <amoinier@student.42.fr>
2016-04-18 21:05:00 +0200

    debut design

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/font.css" charset="utf-8">
+		<link rel="stylesheet" href="css/header.css" charset="utf-8">
+		<title><?= $title?></title>
+	</head>
+	<body>
+		<div class="bandeau">
+			<span id='title'>InstaSnap</span>
+			<span class="button"><a href="#">Logout</a></span>
+			<span class="button"><a href="#">Take a snap!</a></span>
+			<span class="button"><a href="#">Home</a></span>
+		</div>
