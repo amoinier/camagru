f59c5c8fe045976c9805bfa8d38dd6cea07bcbab
Alex MOINIER <amoinier@student.42.fr>
2016-04-19 13:03:35 +0200

    design

--- header.php
+++ header.php
@@ -9,7 +9,9 @@
 	<body>
 		<div class="bandeau">
 			<span id='title'>InstaSnap</span>
-			<span class="button"><a href="#">Logout</a></span>
-			<span class="button"><a href="#">Take a snap!</a></span>
-			<span class="button"><a href="#">Home</a></span>
+			<div class="linebutton">
+				<span class="button"><a href="#">Logout</a></span>
+				<span class="button"><a href="#">Take a snap!</a></span>
+				<span class="button"><a href="#">Home</a></span>
+			</div>
 		</div>
